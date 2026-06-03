b64666a3bd5c1bb343476548b47ddfa7aed0dff1
Artem <artem@callkeeper.ru>
2026-06-04 01:34:50 +0300

    Fix unfinished road CI build

--- chicken-road-unfinished/src/engine/engine.ts
+++ chicken-road-unfinished/src/engine/engine.ts
@@ -51,7 +51,9 @@ export class CreationEngine extends Application {
     await Assets.loadBundle("preload");
 
     // List all existing bundles names
-    const allBundles = manifest.bundles.map((item) => item.name);
+    const allBundles = (manifest.bundles as Array<{ name: string }>).map(
+      (item) => item.name,
+    );
     // Start up background loading of all bundles
     Assets.backgroundLoadBundle(allBundles);
   }
